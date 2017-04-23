56b02b01627e5ec5be21e64ce3e356de9281494c
Lam Kimserey <kimserey.lam@groomgy.com>
2017-04-23 08:56:42 +0800

    filter tests

---
+++ AuthorizationSamples.FiltersTest/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationSamples.FiltersTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        [HelloFilter]
+        public string Get1()
+        {
+            return "works";
+        }
+
+        [HttpGet("2")]
+        [Hello2Filter]
+        public string Get2()
+        {
+            return "works";
+        }
+    }
+}
---
+++ AuthorizationSamples.FiltersTest/Filters.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Filter doc
+// https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/filters
+//
+namespace AuthorizationSamples.FiltersTest
+{
+    // ActionFilterAttribute hides the empty implementations which would have been needed 
+    // when implementing straight IActionFilter
+    public class HelloFilterAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("Hello");
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+            Console.WriteLine("Bye bye");
+        }
+    }
+
+    public class Hello2FilterAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        { }
+    }
+
+
+}
